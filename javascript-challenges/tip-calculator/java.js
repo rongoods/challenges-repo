4955cfd5dab6cd91a1dbcb915be75ac7ced64dd7
Ron McCoy <rongoods@gmail.com>
2023-09-28 11:50:05 +0200

    tip calculator completed

---
+++ javascript-challenges/tip-calculator/java.js
@@ -0,0 +1,6 @@
+const costOfMeal = 10;
+const tipPercentage = 0.2;
+const tipAmount = costOfMeal * tipPercentage;
+const totalCost = costOfMeal + tipAmount;
+
+console.log("The total cost of your meal is: " + totalCost + "euros");
