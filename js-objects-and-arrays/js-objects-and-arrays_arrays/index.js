abcd1eae029c3b1feb4149877c865a2dcec2828e
Ron McCoy <rongoods@gmail.com>
2023-10-02 11:05:58 +0200

    4/10 problems solved

--- js-objects-and-arrays/js-objects-and-arrays_arrays/index.js
+++ js-objects-and-arrays/js-objects-and-arrays_arrays/index.js
@@ -25,12 +25,18 @@ console.log(nestedArray);
 
 const numbers = [20, 10, 50];
 
-const firstNumber = "change me";
+const firstNumber = numbers[0];
+
+console.log(firstNumber);
 
 // EXERCISE 4
 // Change the fruit stored at index 0 of fruits to mango.
 
 const fruits = ["apple", "banana", "grapefruit"];
+
+fruits.unshift("mango");
+
+console.log(fruits);
 
 // EXERCISE 5
 // Change the value of nestedNumber to equal the fourth number of nestedNumbers using bracket notation.
