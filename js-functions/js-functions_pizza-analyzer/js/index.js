857312d193018e63ac6682b5798d3b4cb99c13da
Ron McCoy <rongoods@gmail.com>
2023-10-05 17:22:21 +0200

    pizza analyzer complete

--- js-functions/js-functions_pizza-analyzer/js/index.js
+++ js-functions/js-functions_pizza-analyzer/js/index.js
@@ -8,23 +8,47 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 
 const output = document.querySelector('[data-js="output"]');
 
+let diameter1 = pizzaInput1.value;
+let diameter2 = pizzaInput2.value;
+
 pizzaInput1.addEventListener("input", () => {
   // write your code here
+  diameter1 = pizzaInput1.value;
+  calculatePizzaGain(diameter1, diameter2);
+  updatePizzaDisplay(pizza1, diameter1);
+  updateOutputColor(diameter1, diameter2);
 });
 
 pizzaInput2.addEventListener("input", () => {
   // write your code here
+  diameter2 = pizzaInput2.value;
+  calculatePizzaGain(diameter1, diameter2);
+  updatePizzaDisplay(pizza2, diameter2);
+  updateOutputColor(diameter1, diameter2);
 });
 
 // Task 1
 // define the function calculatePizzaGain here
 function calculatePizzaGain(diameter1, diameter2) {
-  pizza1 = Math.PI * diameter1;
-  pizza2 = Math.PI * diameter2;
+  const pizza1 = (diameter1 / 2) ** 2 * Math.PI;
+  const pizza2 = (diameter2 / 2) ** 2 * Math.PI;
+  output.innerHTML = Math.round(((pizza2 - pizza1) / pizza1) * 100);
 }
 
 // Task 2
 // define the function updatePizzaDisplay here
+function updatePizzaDisplay(pizzaElement, newSize) {
+  const result = (newSize / 24) * 100;
+
+  pizzaElement.style.width = result + "px";
+}
 
 // Task 3
 // define the function updateOutputColor here
+function updateOutputColor(size1, size2) {
+  if (size1 <= size2) {
+    outputSection.style.backgroundColor = "var(--green)";
+  } else {
+    outputSection.style.backgroundColor = "var(--red)";
+  }
+}
