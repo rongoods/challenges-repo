96b72952a86e252b7c01f23c78f193f10d868c7c
Ron McCoy <rongoods@gmail.com>
2023-10-18 12:01:31 +0200

    destructuring arrays complete

--- js-modern-syntax/js-modern-syntax_destructuring-arrays/index.js
+++ js-modern-syntax/js-modern-syntax_destructuring-arrays/index.js
@@ -6,6 +6,10 @@ console.clear();
 
 const item = ["Egg", 0.25, 12];
 
+export const [name, price, quantity] = item;
+
+console.log("name: ", name, "price: ", price, "quantity: ", quantity);
+
 // example: export const [value1, value2] = array;
 
 // EXERCISE 2
@@ -13,11 +17,31 @@ const item = ["Egg", 0.25, 12];
 
 const personChris = [12, "Chris", "Owen"];
 
+export const [ageOfChris, firstNameOfChris, lastNameOfChris] = personChris;
+
+console.log(
+  "firstNameOfChris: ",
+  firstNameOfChris,
+  "lastNameOfChris: ",
+  lastNameOfChris,
+  "ageOfChris: ",
+  ageOfChris
+);
+
 // EXERCISE 3
 // Use array destructuring to extract the variables "firstNameOfAlex" and "lastNameOfAlex".
 // Make sure not to have unused variables.
 
 const personAlex = ["Alex", 12, "Main"];
+
+export const [firstNameOfAlex, , lastNameOfAlex] = personAlex;
+
+console.log(
+  "firstNameOfAlex: ",
+  firstNameOfAlex,
+  "lastNameOfAlex: ",
+  lastNameOfAlex
+);
 
 // EXERCISE 4
 // Use array destructuring to extract the last name from the array
@@ -25,11 +49,31 @@ const personAlex = ["Alex", 12, "Main"];
 
 const students = ["Christina", "Jon", "Alexandare"];
 
+export const [, , lastName] = students;
+
+console.log(lastName);
+
 // EXERCISE 5
 // Use array destructuring to extract all of the names from this nested array.
 // Assign the given order of students to variables called "student1" to "student5".
 
 const nestedStudents = ["Chris", ["Ahmad", "Antigoni"], ["Toby", "Sam"]];
+
+export const [student1, [student2, student3], [student4, student5]] =
+  nestedStudents;
+
+console.log(
+  "student1: ",
+  student1,
+  "student2: ",
+  student2,
+  "student3: ",
+  student3,
+  "student4: ",
+  student4,
+  "student5: ",
+  student5
+);
 
 // EXERCISE 6
 // Spread the values of the values array into the function call of the add function.
@@ -37,7 +81,7 @@ const nestedStudents = ["Chris", ["Ahmad", "Antigoni"], ["Toby", "Sam"]];
 
 const values = [1, 6, 7, 9, 12, 5, 4];
 
-const result = add(1); // Spread values inside this function call
+export const result = add(...values); // Spread values inside this function call
 
 function add(...values) {
   return values.reduce(
