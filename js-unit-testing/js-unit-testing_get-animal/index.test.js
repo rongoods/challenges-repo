e36ffe3c3a5d7696d66c7b33e2db9a846ec26eaf
Ron McCoy <rongoods@gmail.com>
2023-10-19 11:33:46 +0200

    unit testing animal complete

--- js-unit-testing/js-unit-testing_get-animal/index.test.js
+++ js-unit-testing/js-unit-testing_get-animal/index.test.js
@@ -10,5 +10,5 @@ test("getAnimals() returns a string 'I like [animalPluralName]!' if called with 
 });
 test("getAnimals() returns a string 'I do not like animals at all!' if called without arguments.", () => {
   const result = getAnimal();
-  expect(result).toBe("I do not like animals at all!");
+  expect(result).toBe(`I do not like animals at all!`);
 });
