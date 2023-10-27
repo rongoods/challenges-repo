c0d08d35e1839b79aa110e362ed3dc9f03ff64e2
Ron McCoy <rongoods@gmail.com>
2023-10-27 12:07:33 +0200

    complete react arrays fruits

---
+++ react-with-arrays/react-with-arrays_fruits/src/lib/fruits.js
@@ -0,0 +1,31 @@
+export const fruits = [
+  {
+    id: 1,
+    name: "Banana",
+    color: "yellow",
+  },
+
+  {
+    id: 2,
+    name: "Apple",
+    color: "red",
+  },
+
+  {
+    id: 3,
+    name: "Pear",
+    color: "green",
+  },
+
+  {
+    id: 4,
+    name: "Fig",
+    color: "purple",
+  },
+
+  {
+    id: 5,
+    name: "Grape",
+    color: "green",
+  },
+];
