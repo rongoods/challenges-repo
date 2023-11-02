abef0ac37eb54027a2d6bc9a741b648fb3739b69
Ron McCoy <rongoods@gmail.com>
2023-11-02 15:40:59 +0100

    complete react state counter

--- react-state/react-state_counter/src/App.js
+++ react-state/react-state_counter/src/App.js
@@ -1,27 +1,27 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let count = 0;
+  //   let count = 0;
+  const [count, setCount] = useState(0);
+
+  function IncrementCount() {
+    setCount(count + 1);
+  }
+
+  function DecrementCount() {
+    setCount(count - 1);
+  }
 
   return (
     <div className="container">
       <h1>{count}</h1>
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={DecrementCount}>
           -
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={IncrementCount}>
           +
         </button>
       </div>
