88841847634927b0b243e96f229fe742a569be02
Ron McCoy <rongoods@gmail.com>
2023-11-14 13:51:20 +0100

    complete data fetching iss tracker

--- react-data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react-data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,44 +1,48 @@
 import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
+  const { data, mutate, isLoading, error } = useSWR(URL);
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>...loading...</div>;
+  //   const [coords, setCoords] = useState({
+  //     longitude: 0,
+  //     latitude: 0,
+  //   });
 
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  //   async function getISSCoords() {
+  //     try {
+  //       const response = await fetch(URL);
+  //       if (response.ok) {
+  //         const data = await response.json();
+  //         setCoords({ longitude: data.longitude, latitude: data.latitude });
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
+  //   useEffect(() => {
+  //     const timer = setInterval(() => {
+  //       getISSCoords();
+  //     }, 5000);
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+  //     return () => {
+  //       clearInterval(timer);
+  //     };
+  //   }, []);
 
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Map longitude={data.longitude} latitude={data.latitude} />
       <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onRefresh={() => mutate()}
       />
     </main>
   );
--- react-data-fetching/react-data-fetching_iss-tracker/pages/_app.js
+++ react-data-fetching/react-data-fetching_iss-tracker/pages/_app.js
@@ -1,10 +1,13 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
+
+const fetcher = (...coords) => fetch(...coords).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <SWRConfig value={{ fetcher, refreshInterval: 5000 }}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </SWRConfig>
   );
 }
