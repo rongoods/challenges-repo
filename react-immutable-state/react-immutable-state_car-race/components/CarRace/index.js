d74a005369ff744c63d4a249b0eb0c9caccec014
Ron McCoy <rongoods@gmail.com>
2023-11-20 15:22:55 +0100

    ::

--- react-immutable-state/react-immutable-state_car-race/components/CarRace/index.js
+++ react-immutable-state/react-immutable-state_car-race/components/CarRace/index.js
@@ -15,10 +15,47 @@ export default function CarRace() {
   const [cars, setCars] = useState(initialCars);
 
   function moveCar(clickedCar) {
-    const coveredDistance = getRandomDistance();
-    console.log("clickedCar", clickedCar);
-    console.log("coveredDistance", coveredDistance);
+    // const coveredDistance = getRandomDistance();
+    // console.log("clickedCar", clickedCar);
+    // console.log("coveredDistance", coveredDistance);
+
+    setCars((prevDistances) => {
+        return prevDistances.map((prevDistance) => {
+            if (prevDistance.lastDistance !== prevDistance){
+                return prevDistance;
+            }
+            return {
+                ...prevDistance, 
+                distances: prevDistance.
+            }
+        })
+    })
   }
+
+      // setSeasons((prevSeasons) => {
+    //   return prevSeasons.map((prevSeason) => {
+    //     if (prevSeason.number !== seasonNumber) {
+    //       return prevSeason;
+    //     }
+    //     return {
+    //       ...prevSeason,
+    //       episodes: prevSeason.episodes.map((prevEpisode) => {
+    //         if (prevEpisode.number !== episodeNumber) {
+    //           return prevEpisode;
+    //         }
+    //         return {
+    //           ...prevEpisode,
+    //           hasSeen: !prevEpisode.hasSeen,
+    //         };
+    //       }),
+    //     };
+    //   });
+    // });
+
+
+
+
+
 
   const winner = cars.find((car) => car.position.x >= finishLine);
 
