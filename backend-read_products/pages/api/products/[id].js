1312f06350b0d62414785cb2b13e62c48509e744
Ron McCoy <rongoods@gmail.com>
2023-11-22 15:52:09 +0100

    complete backend read products

--- backend-read_products/pages/api/products/[id].js
+++ backend-read_products/pages/api/products/[id].js
@@ -1,9 +1,11 @@
-import { products } from "../../../lib/products";
+import dbConnect from "@/db/dbConnect";
+import Product from "@/db/Product";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
-  const product = products.find((product) => product.id === id);
+  const product = await Product.findById(id);
 
   if (!product) {
     return response.status(404).json({ status: "Not Found" });
